3bf655dfa7727007adc38e8b108689838ed5cd84
arthurCapistrano <arthur.capistrano12@gmail.com>
2026-04-04 23:04:18 -0300

    add: hometeste path. It still needs to garantee the authetication path

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,36 @@
+// src/routes/index.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import HomeTeste from "@/pages/HomeTeste"; 
 import PrivateRoute from "./PrivateRoute";
+import { Layout } from "@/components/Layout";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Rota de Login (Totalmente Limpa) */}
         <Route path="/login" element={<Login />} />
 
+        {/* Rota de Teste (Pública, mas com a moldura do sistema) */}
+        <Route
+          path="/teste"
+          element={
+            <Layout>
+              <HomeTeste />
+            </Layout>
+          }
+        />
+
+        {/* Rota Oficial (Protegida) */}
         <Route
           path="/"
           element={
             <PrivateRoute>
-              <Home />
+              <Layout>
+                <Home />
+              </Layout>
             </PrivateRoute>
           }
         />
