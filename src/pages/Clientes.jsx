7ddf3fa28d6e5766c6a9d6d7f8d69d0c42353c7c
Joaoamlp <joaoamlp@gmail.com>
2026-04-07 16:54:18 -0300

    refactor: making changes asked

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -52,7 +52,6 @@ export default function Clientes() {
 
     carregarDados();
 
-   
     return () => {
       componenteMontado = false;
     };
@@ -72,19 +71,11 @@ export default function Clientes() {
         <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-8 w-full max-w-6xl mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-center mb-6 gap-4">
             <div className="flex items-center gap-3">
-              <svg
-                className="w-8 h-8 text-gray-700"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                />
-              </svg>
+              <img
+                src="/clientes-ativado.png"
+                alt="Ícone de Clientes"
+                className="w-10 h-10 object-contain"
+              />
               <h2 className="text-3xl font-Outfit font-extralight text-[#404040] tracking-tight">
                 Clientes
               </h2>
@@ -113,29 +104,19 @@ export default function Clientes() {
               </div>
 
               <button className="bg-[#9be0ee] text-white rounded-full px-5 py-2.5 flex items-center gap-2 text-sm font-Outfit font-light hover:bg-[#7ecbda] transition-colors whitespace-nowrap">
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
+                <img
+                  src="public\add-star.png"
+                  alt="estrelapequena"
+                />
                 Cadastrar cliente
               </button>
             </div>
           </div>
-
-          <div className="overflow-x-auto">
+          <div className="overflow-x-auto border border-gray-100 rounded-xl">
             <table className="w-full text-center border-collapse">
               <thead>
-                <tr className="bg-[#C9EAF6] text-[#4696AD] text-sm">
-                  <th className="py-4 px-4 font-Outfit font-light rounded-tl-xl w-1/6">
+                <tr className="bg-[#C9EAF6] text-[#4696AD] text-sm border-b border-[#a9d8eb]">
+                  <th className="py-4 px-4 font-Outfit font-light w-1/6">
                     Cliente
                   </th>
                   <th className="py-4 px-4 font-Outfit font-light w-1/6">
@@ -147,8 +128,7 @@ export default function Clientes() {
                   <th className="py-4 px-4 font-Outfit font-light w-1/6">
                     Status
                   </th>
-                  {/* Adicionado text-center aqui para garantir o alinhamento do título */}
-                  <th className="py-4 px-4 font-Outfit font-light rounded-tr-xl w-1/6 text-center">
+                  <th className="py-4 px-4 font-Outfit font-light w-1/6 text-center">
                     Opções
                   </th>
                 </tr>
@@ -159,8 +139,8 @@ export default function Clientes() {
                     key={cliente.id}
                     className={
                       index % 2 === 0
-                        ? "bg-white"
-                        : "bg-[#F4F4F4] border-y border-gray-100"
+                        ? "bg-white border-b border-gray-100"
+                        : "bg-[#F4F4F4] border-b border-gray-100"
                     }
                   >
                     <td className="py-5 px-4 text-gray-600 font-Outfit font-light">
@@ -174,7 +154,7 @@ export default function Clientes() {
                     </td>
                     <td className="py-5 px-4">
                       <span
-                        className={`px-8 py-1.5 rounded-full text-xs font-Outfit font-light text-white ${cliente.status ? "bg-[#aedd57]" : "bg-gray-400"}`}
+                        className={`px-8 py-1.5 rounded-full text-xs font-Outfit font-light text-white ${cliente.status ? "bg-[#B4D64E]" : "bg-[#D9D9D9] text-[#414040]"}`}
                       >
                         {cliente.status ? "Ativo" : "Inativo"}
                       </span>
