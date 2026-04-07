ccbb3ccf9a4c209a4495f1dc58ae29d6d1a14cb6
Joaoamlp <joaoamlp@gmail.com>
2026-04-07 15:36:26 -0300

    refactor: clinetesService

--- src/services/clientesService.js
+++ src/services/clientesService.js
@@ -3,24 +3,18 @@ import { getMe } from "./authService";
 
 export const getClientes = async () => {
   try {
-    const response = await api.get("/clientes");
-    const todosClientes = response.data;
-
     const usuarioLogado = await getMe();
 
     if (!usuarioLogado) return [];
 
-    // Filtra comparando o fabrico_id do Cliente com o fabrico_id do Usuário
-    const clientesFiltrados = todosClientes.filter(
-      (cliente) =>
-        String(cliente.fabrico_id) === String(usuarioLogado.fabrico_id),
+    const response = await api.get(
+      `clientes/fabrico/${usuarioLogado.fabrico_id}`,
     );
 
-    return clientesFiltrados;
+    return response.data;
   } catch (error) {
     console.error("Erro ao buscar clientes:", error);
 
-    // A MUDANÇA É AQUI: Repassamos o erro adiante para que a tela consiga capturá-lo!
     throw error;
   }
 };
