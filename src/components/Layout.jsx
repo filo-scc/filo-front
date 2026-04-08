be5062bedf6d5369b63095b1486b1c056687f206
Gheyson <gheysonmelo@hotmail.com>
2026-04-08 09:32:34 -0300

    refactor: repositioning footer text

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,32 +3,34 @@ import { Sidebar } from "./Sidebar";
 
 export function Layout({ children }) {
   return (
-    <div 
-      className="min-h-screen flex" 
-      style={{ 
+    <div
+      className="min-h-screen flex flex-col"
+      style={{
         background: "linear-gradient(to bottom, #F4F4F4 25%, #C7E9F5 100%)",
-        backgroundAttachment: "fixed" 
-      }}>
-      
-      {/* Sidebar Fixa*/}
-      <div className="fixed inset-y-0 left-0 z-40">
-        <Sidebar />
-      </div>
-      
-      {/* Área de Conteúdo*/}
-      <div className="flex-1 ml-[219px] flex flex-col">
-        <Header />
-        
-        <main className="flex-1 p-6">
-          {children}
-        </main>
+        backgroundAttachment: "fixed",
+      }}
+    >
+      {/* Conteúdo principal (Sidebar + Área) */}
+      <div className="flex flex-1">
+        {/* Sidebar Fixa */}
+        <div className="fixed inset-y-0 left-0 z-40">
+          <Sidebar />
+        </div>
 
-        <footer className="w-full h-[117px] flex items-center justify-center p-10 mt-auto">
-          <p className="text-[#4696AD] text-sm font-medium">
-            Filo® | Onde negócios fluem, resultados acontecem
-          </p>
-        </footer>
+        {/* Área de Conteúdo */}
+        <div className="flex-1 ml-[219px] flex flex-col">
+          <Header />
+
+          <main className="flex-1 p-6">{children}</main>
+        </div>
       </div>
+
+      {/* Footer */}
+      <footer className="w-full h-[117px] flex items-center justify-center p-10">
+        <p className="text-[#4696AD] text-sm font-medium">
+          Filo® | Onde negócios fluem, resultados acontecem
+        </p>
+      </footer>
     </div>
   );
 }
