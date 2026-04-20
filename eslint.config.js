376886aedcbbd7539caf13fbf09cee2e23ac8862
arthurCapistrano <arthur.capistrano12@gmail.com>
2026-04-20 14:34:56 -0300

    add: prettier and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
