4f3c51a260af4a76ec80d56bc99f27dd7f780914
Erico <erico.chen@hotmail.com>
2026-04-25 21:09:20 -0300

    Revert Merge branch 'master' into feature/clientes

--- src/components/produtos/ProdutoDetalhesHeader.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProdutoDetalhesHeader({ title }) {
-    return (
-        <div className="flex items-center gap-3">
-            <img src="/produtos-ativado.png" className="w-[30px] h-[30px] object-contain" />
-            <h2 className="text-[30px] font-Outfit font-light text-[#404040]">{title}</h2>
-        </div>
-    );
-}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,4 @@
+// src/routes/index.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
@@ -8,167 +9,163 @@ import Faccoes from "@/pages/Faccoes";
 import ClienteDetalhes from "../pages/ClienteDetalhes";
 import ClientesCadastrar from "../pages/ClientesCadastrar";
 import ClientesEditar from "../pages/ClientesEditar";
-import FaccaoDetalhes from "../pages/FaccaoDetalhes";
-import ProdutoDetalhes from "../pages/ProdutoDetalhes";
 
 function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rota de Login (Totalmente Limpa) */}
+        <Route path="/login" element={<Login />} />
 
-                {/* Rota Oficial (Protegida) */}
-                {/* Envolvendo a Home */}
-                <Route
-                    path="/"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <Home />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/clientes"
-                    element={
-                        <PrivateRoute>
-                            <Cliente />
-                        </PrivateRoute>
-                    }
-                ></Route>
+        {/* Rotas Protegidas */}
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Home />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/clientes"
+          element={
+            <PrivateRoute>
+              <Cliente />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/clientes/:id"
+          element={
+            <PrivateRoute>
+              <ClienteDetalhes />
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/clientes/cadastrar"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <ClientesCadastrar />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/clientes/cadastrar"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <ClientesCadastrar />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/clientes/:id/editar"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <ClientesEditar />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/clientes/:id/editar"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <ClientesEditar />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/clientes/:id"
-                    element={
-                        <PrivateRoute>
-                            <ClienteDetalhes />
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/clientes/:id"
+          element={
+            <PrivateRoute>
+              <ClienteDetalhes />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/produtos/:id"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <ProdutoDetalhes />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/produtos/:id"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <ProdutoDetalhes />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        {/* Envolvendo a página de Facções */}
+        <Route
+          path="/faccoes"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Faccoes />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/faccoes"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <Faccoes />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/faccoes/:id"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <FaccaoDetalhes />
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        {/* Telas provisórias com a header + sidebar por enquanto que não são implementadas */}
+        <Route
+          path="/pedidos"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
+                  Página de Pedidos em construção...
+                </div>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                {/* Telas provisórias com a header + sidebar por enquanto que não são implementadas */}
-                <Route
-                    path="/pedidos"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
-                                    Página de Pedidos em construção...
-                                </div>
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/produtos"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
+                  Página de Produtos em construção...
+                </div>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/produtos"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
-                                    Página de Produtos em construção...
-                                </div>
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/estoque"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
+                  Página de Estoque em construção...
+                </div>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/estoque"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
-                                    Página de Estoque em construção...
-                                </div>
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
+        <Route
+          path="/financeiro"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
+                  Página do Financeiro em construção...
+                </div>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
 
-                <Route
-                    path="/financeiro"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
-                                    Página do Financeiro em construção...
-                                </div>
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
-
-                <Route
-                    path="/configuracoes"
-                    element={
-                        <PrivateRoute>
-                            <Layout>
-                                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
-                                    Página de Configurações em construção...
-                                </div>
-                            </Layout>
-                        </PrivateRoute>
-                    }
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route
+          path="/configuracoes"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <div className="flex justify-center items-center h-full text-gray-500 text-2xl font-light">
+                  Página de Configurações em construção...
+                </div>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
