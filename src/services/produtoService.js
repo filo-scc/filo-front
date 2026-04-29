675eaaafefef9fbd16855f806ee2c1fdd6fb03f4
Gheyson <gheysonmelo@hotmail.com>
2026-04-29 11:57:39 -0300

    lint: executing prettier format script

--- src/services/produtoService.js
+++ src/services/produtoService.js
@@ -1,21 +1,21 @@
-import api from './api';
+import api from "./api";
 
 export const getProdutos = async () => {
-  try {
-    const response = await api.get('/produtos');
-    return response.data;
-  } catch (error) {
-    console.error("Erro ao buscar produtos:", error);
-    throw error;
-  }
+    try {
+        const response = await api.get("/produtos");
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        throw error;
+    }
 };
 
 export const getProdutosByFabrico = async (fabricoId) => {
-  try {
-    const response = await api.get(`/produtos/fabrico/${fabricoId}`);
-    return response.data;
-  } catch (error) {
-    console.error("Erro ao buscar produtos do fabrico:", error);
-    throw error;
-  }
+    try {
+        const response = await api.get(`/produtos/fabrico/${fabricoId}`);
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar produtos do fabrico:", error);
+        throw error;
+    }
 };
