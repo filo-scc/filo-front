471f2f648cc644fc053bbca6710d302b7b5f25bc
Erico <erico.chen@hotmail.com>
2026-04-30 18:09:45 -0300

    [Fix]: ajusta as bordas dos inputs (mais retangular) em editar e cadastrar clientes

--- src/components/FloatingLabelInput.jsx
+++ src/components/FloatingLabelInput.jsx
@@ -22,16 +22,16 @@ export default function FloatingLabelInput({
         value={value}
         onChange={onChange}
         className={`
-          w-full h-[39px] border border-[#D3D3D3] rounded-[25px] text-sm text-[#404040]
+          w-full h-[39px] border border-[#D3D3D3] rounded-[10px] text-sm text-gray-600
           leading-[39px] focus:outline-none
           transition-colors bg-white
-          ${endAdornment ? "pl-4 pr-10" : "px-4"}
+          ${endAdornment ? "pl-3 pr-10" : "px-3"}
           ${inputClassName}
         `}
       />
       <label
         className={`
-          absolute left-4 bg-white px-1 text-gray-400 transition-all duration-200 pointer-events-none
+          absolute left-3 bg-white px-1 text-gray-400 transition-all duration-200 pointer-events-none
           ${
             hasValue
               ? "top-0 -translate-y-1/2 text-xs text-[#898C8F]"
