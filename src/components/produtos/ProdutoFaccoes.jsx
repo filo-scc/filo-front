2834dec79b042fd8170d4f9545d5a1669e11ec92
Lucas de Holanda <lhbs@cesar.school>
2026-05-09 11:22:39 -0300

    Add: estilo na scroll bar padrão

--- src/components/produtos/ProdutoFaccoes.jsx
+++ src/components/produtos/ProdutoFaccoes.jsx
@@ -73,134 +73,164 @@ const ProdutoFaccoes = ({ isOpen, onClose, produtoId }) => {
     if (!isOpen) return null;
 
     return (
-        <div
-            className="fixed inset-0 bg-black/35 backdrop-blur-sm flex items-center justify-center z-[999]"
-            onClick={onClose}
-        >
+        <>
+            <style>
+                {`
+                .scrollbar-sutil::-webkit-scrollbar {
+                    width: 4px;
+                }
+
+                .scrollbar-sutil::-webkit-scrollbar-track {
+                    background: transparent;
+                }
+
+                .scrollbar-sutil::-webkit-scrollbar-thumb {
+                    background-color: #d1d5db;
+                    border-radius: 10px;
+                }
+
+                /* ISSO AQUI É O CERTO */
+                .scrollbar-sutil {
+                    scrollbar-gutter: stable;
+                }
+            `}
+            </style>
+
             <div
-                className="w-[900px] bg-white rounded-[24px] px-10 pt-10 pb-8 relative"
-                onClick={(e) => e.stopPropagation()}
+                className="fixed inset-0 bg-black/35 backdrop-blur-sm flex items-center justify-center z-[999]"
+                onClick={onClose}
             >
-                {/* HEADER */}
-                <div className="flex justify-between items-center mb-8">
-                    <div className="flex items-center gap-3">
-                        <img
-                            src="/maquina+-costura-icone-preto.png"
-                            alt="Ícone máquina de costura"
-                            className="w-8 h-8 object-contain"
-                        />
+                <div
+                    className="w-[900px] bg-white rounded-[24px] px-10 pt-10 pb-8 relative"
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    {/* HEADER */}
+                    <div className="flex justify-between items-center mb-8">
+                        <div className="flex items-center gap-3">
+                            <img
+                                src="/maquina+-costura-icone-preto.png"
+                                alt="Ícone máquina de costura"
+                                className="w-8 h-8 object-contain"
+                            />
 
-                        <h2 className="text-[26px] font-Outfit font-light text-[#404040]">
-                            Selecione a facção
-                        </h2>
-                    </div>
+                            <h2 className="text-[26px] font-Outfit font-light text-[#404040]">
+                                Selecione a facção
+                            </h2>
+                        </div>
 
-                    {/* DROPDOWN */}
-                    <div className="relative">
-                        <button
-                            onClick={() => setDropdownOpen(!dropdownOpen)}
-                            className="border border-[#D9D9D9] rounded-[12px] bg-white px-4 py-3 min-w-[180px] flex items-center justify-between text-[15px] font-Outfit text-[#404040]"
-                        >
-                            <span className="text-[#7B7D80]">{getOrderLabel()}</span>
-
-                            <svg
-                                className={`w-4 h-4 text-gray-400 transition-transform duration-300 ${
-                                    dropdownOpen ? "rotate-180" : ""
-                                }`}
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
+                        {/* DROPDOWN */}
+                        <div className="relative">
+                            <button
+                                onClick={() => setDropdownOpen(!dropdownOpen)}
+                                className="border border-[#D9D9D9] rounded-[12px] bg-white px-4 py-3 min-w-[180px] flex items-center justify-between text-[15px] font-Outfit text-[#404040]"
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M19 9l-7 7-7-7"
-                                />
-                            </svg>
-                        </button>
+                                <span className="text-[#7B7D80]">{getOrderLabel()}</span>
 
-                        <div
-                            className={`absolute top-14 right-0 w-[180px] bg-white border border-[#D9D9D9] rounded-[12px] overflow-hidden z-[999] shadow-md origin-top transition-all duration-300
+                                <svg
+                                    className={`w-4 h-4 text-gray-400 transition-transform duration-300 ${
+                                        dropdownOpen ? "rotate-180" : ""
+                                    }`}
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M19 9l-7 7-7-7"
+                                    />
+                                </svg>
+                            </button>
+
+                            <div
+                                className={`absolute top-14 right-0 w-[180px] bg-white border border-[#D9D9D9] rounded-[12px] overflow-hidden z-[999] shadow-md origin-top transition-all duration-300
                                 ${
                                     dropdownOpen
                                         ? "opacity-100 scale-y-100 visible"
                                         : "opacity-0 scale-y-95 invisible pointer-events-none"
                                 }
                             `}
-                        >
-                            <div
-                                className={dropdownItemClass}
-                                onClick={() => handleSelectOrder("nome")}
                             >
-                                <div className="text-[#7B7D80]">Nome (A-Z)</div>
-                            </div>
+                                <div
+                                    className={dropdownItemClass}
+                                    onClick={() => handleSelectOrder("nome")}
+                                >
+                                    <div className="text-[#7B7D80]">Nome (A-Z)</div>
+                                </div>
 
-                            <div
-                                className={dropdownItemClass}
-                                onClick={() => handleSelectOrder("menor-preco")}
-                            >
-                                <div className="text-[#7B7D80]">Menor preço</div>
-                            </div>
+                                <div
+                                    className={dropdownItemClass}
+                                    onClick={() => handleSelectOrder("menor-preco")}
+                                >
+                                    <div className="text-[#7B7D80]">Menor preço</div>
+                                </div>
 
-                            <div
-                                className={dropdownItemClass}
-                                onClick={() => handleSelectOrder("maior-preco")}
-                            >
-                                <div className="text-[#7B7D80]">Maior preço</div>
+                                <div
+                                    className={dropdownItemClass}
+                                    onClick={() => handleSelectOrder("maior-preco")}
+                                >
+                                    <div className="text-[#7B7D80]">Maior preço</div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                {/* TABELA */}
-                <div className="border border-[#EAEAEA] rounded-[16px] overflow-hidden">
-                    {/* HEADER */}
-                    <div className="grid grid-cols-3 bg-[#DFF5FF] px-6 py-5 font-Outfit text-[#4F9DB8]">
-                        <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
-                            Facção
-                        </div>
-
-                        <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
-                            Preço
-                        </div>
-
-                        <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
-                            Possui pedido no momento
-                        </div>
-                    </div>
-
-                    {/* ROWS */}
-                    <div className="max-h-[360px] overflow-y-auto">
-                        {faccoesOrdenadas.map((faccao, index) => (
-                            <div
-                                key={faccao.id}
-                                className={`
-                                    grid grid-cols-3 px-6 py-5 items-center
-                                    ${index % 2 === 0 ? "bg-white" : "bg-[#FAFAFA]"}
-                                    ${index !== faccoesOrdenadas.length - 1 ? "border-b border-[#FAFAFA]" : ""}
-                                `}
-                            >
-                                <div className="flex justify-center items-center text-center font-Outfit text-[16px] font-light text-[#404040] px-2">
-                                    <span className="max-w-[180px] break-words">{faccao.nome}</span>
-                                </div>
-                                <div className="flex justify-center font-Outfit text-[16px] font-light text-[#404040]">
-                                    {faccao.preco !== null
-                                        ? `R$${Number(faccao.preco).toFixed(2).replace(".", ",")}`
-                                        : "-"}
-                                </div>
-
-                                <div className="flex justify-center">
-                                    <span className="bg-[#D9D9D9] rounded-full w-[109px] h-[19px] text-[12px] font-Outfit font-light text-[#404040] inline-flex items-center justify-center">
-                                        {faccao.possuiPedido ? "Sim" : "Não"}
-                                    </span>
-                                </div>
+                    {/* TABELA */}
+                    <div className="border border-[#EAEAEA] rounded-[16px] overflow-hidden">
+                        {/* HEADER */}
+                        <div className="grid grid-cols-3 bg-[#DFF5FF] px-6 py-5 font-Outfit text-[#4F9DB8]">
+                            <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
+                                Facção
                             </div>
-                        ))}
+
+                            <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
+                                Preço
+                            </div>
+
+                            <div className="flex justify-center text-[16px] font-Outfit font-light text-[#4696AD] leading-none">
+                                Possui pedido no momento
+                            </div>
+                        </div>
+
+                        {/* ROWS */}
+
+                        <div className="max-h-[360px] overflow-y-auto scrollbar-sutil">
+                            {faccoesOrdenadas.map((faccao, index) => (
+                                <div
+                                    key={faccao.id}
+                                    className={`
+                                            grid grid-cols-[1fr_1fr_1fr_12px] px-6 py-5 items-center
+                                            ${index % 2 === 0 ? "bg-white" : "bg-[#FAFAFA]"}
+                                            ${index !== faccoesOrdenadas.length - 1 ? "border-b border-[#FAFAFA]" : ""}
+                                        `}
+                                >
+                                    <div className="flex justify-center items-center text-center font-Outfit text-[16px] font-light text-[#404040] px-2">
+                                        <span className="max-w-[180px] break-words">
+                                            {faccao.nome}
+                                        </span>
+                                    </div>
+
+                                    <div className="flex justify-center font-Outfit text-[16px] font-light text-[#404040]">
+                                        {faccao.preco !== null
+                                            ? `R$${Number(faccao.preco).toFixed(2).replace(".", ",")}`
+                                            : "-"}
+                                    </div>
+
+                                    <div className="flex justify-center">
+                                        <span className="bg-[#D9D9D9] rounded-full w-[109px] h-[19px] text-[12px] font-Outfit font-light text-[#404040] inline-flex items-center justify-center">
+                                            {faccao.possuiPedido ? "Sim" : "Não"}
+                                        </span>
+                                    </div>
+
+                                    <div />
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
