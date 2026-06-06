e0f7a74892ec0688060f0ddd72bb3420a477a800
ThiagoManguinho <tmrs@cesar.school>
2026-06-06 18:56:47 -0300

    refactor: alterado a distania entre os cards e alterado a distancia do icone para a borda

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import React, { useEffect, useRef, useState } from "react";
-import { getEtapasByFabrico } from "../services/etapasService";
+import { getAllEtapasByFabricoId } from "../services/etapaService";
 import { getFabricoById } from "../services/fabricoService";
 import { getFichaTecnicaByFabrico } from "../services/fichasTecnicasService";
 import { getMe } from "../services/authService";
@@ -50,7 +50,7 @@ export default function Home() {
             setProducaoSobDemanda(fabrico?.fabricacao_sob_demanda === true);
 
             const [etapas, fichasTecnicas] = await Promise.all([
-                getEtapasByFabrico(fId),
+                getAllEtapasByFabricoId(fId),
                 getFichaTecnicaByFabrico(fId),
             ]);
 
@@ -263,7 +263,7 @@ export default function Home() {
                                         onDragOver={handleDragOver}
                                         onDrop={(e) => handleDrop(e, coluna.id)}
                                     >
-                                        <div className="flex justify-between items-center mb-4 px-1 shrink-0">
+                                        <div className="flex justify-between items-center mb-4 px-3 pt-3 shrink-0">
                                             <h2 className="font-normal text-base text-[#404040] flex items-center gap-2">
                                                 <img
                                                     src={coluna.icone?.link || ""}
@@ -281,7 +281,7 @@ export default function Home() {
                                             </button>
                                         </div>
 
-                                        <div className="flex-1 overflow-y-auto pr-1 pb-2 flex flex-col gap-1 min-h-0 custom-scrollbar">
+                                        <div className="flex-1 overflow-y-auto pr-1 pb-3 pl-3 flex flex-col gap-3 min-h-0 custom-scrollbar">
                                             {coluna.fichas.map((ficha) => {
                                                 const parceirosVinculados =
                                                     ficha.produto?.parceiro_produto || [];
--- src/services/etapasService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getEtapasByFabrico = async (fabricoId) => {
-    const response = await api.get(`/etapas/fabrico/${fabricoId}`);
-    return response.data;
-};
